3db0581e8b281efc9ea4fe4f15d25f1f94a3e6f0
kirilla <116667638+KirillStolbov@users.noreply.github.com>
2024-04-09 21:06:09 +0300

    bloc finalize

--- lib/features/pokemons_info/presentation/pokemon_details/bloc/pokemon_details_bloc.dart
+++ lib/features/pokemons_info/presentation/pokemon_details/bloc/pokemon_details_bloc.dart
@@ -1,29 +1,33 @@
 import 'package:bloc/bloc.dart';
 
-import '../../../domain/entities/pokemon_long/pokemon_long_entity.dart';
-import '../../../domain/entities/pokemon_short/pokemon_short_entity.dart';
+import '../../../domain/entities/params/pokemons_long_params.dart';
+import '../../../domain/usecases/read_pokemon_long_usecase.dart';
 import 'pokemon_details_event.dart';
 import 'pokemon_details_state.dart';
 
 class PokemonDetailsBloc extends Bloc<PokemonDetailsEvent, PokemonDetailsState> {
-  PokemonDetailsBloc(this.pokemonId) : super(const PokemonDetailsLoading()) {
+  PokemonDetailsBloc(this._pokemonId, this._readPokemonLongUseCase) : super(const PokemonDetailsLoading()) {
     on<PokemonDetailsFetch>(_onFetch);
   }
 
-  final String pokemonId;
+  final String _pokemonId;
+  final ReadPokemonLongUseCase _readPokemonLongUseCase;
 
   Future<void> _onFetch(PokemonDetailsEvent event, Emitter<PokemonDetailsState> emitter) async {
-    if (state is! PokemonDetailsSuccess) {
-      emitter(const PokemonDetailsLoading());
+    if (state is PokemonDetailsSuccess) return;
 
-      await Future<void>.delayed(const Duration(seconds: 1));
+    emitter(const PokemonDetailsLoading());
 
-      emitter(
-        PokemonDetailsSuccess(
-          PokemonLongEntity(id: pokemonId, frontImageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png', weight: 65, height: 56, types: ['types']),
-          PokemonShortEntity(name: 'Meow', id: 'id'),
-        ),
-      );
+    final response = await _readPokemonLongUseCase.invoke(PokemonLongParams(id: _pokemonId));
+
+    final pokemon = response.data;
+
+    if (pokemon != null) {
+      emitter(PokemonDetailsSuccess(pokemon));
+
+      return;
     }
+
+    emitter(const PokemonDetailsFailure());
   }
 }
