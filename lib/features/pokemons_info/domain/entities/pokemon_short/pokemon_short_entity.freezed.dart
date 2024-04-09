8da84e46fbac52af27bded40f04ef7638fedac82
kirilla <116667638+KirillStolbov@users.noreply.github.com>
2024-04-09 16:53:17 +0300

    pokemon short entity refactoring

--- lib/features/pokemons_info/domain/entities/pokemon_short/pokemon_short_entity.freezed.dart
+++ lib/features/pokemons_info/domain/entities/pokemon_short/pokemon_short_entity.freezed.dart
@@ -14,10 +14,6 @@ T _$identity<T>(T value) => value;
 final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
-PokemonShortEntity _$PokemonShortEntityFromJson(Map<String, dynamic> json) {
-  return _PokemonShortEntity.fromJson(json);
-}
-
 /// @nodoc
 mixin _$PokemonShortEntity {
   String get name => throw _privateConstructorUsedError;
@@ -27,12 +23,9 @@ mixin _$PokemonShortEntity {
 }
 
 /// @nodoc
-@JsonSerializable()
+@JsonSerializable(createFactory: false)
 class _$PokemonShortEntityImpl implements _PokemonShortEntity {
   const _$PokemonShortEntityImpl({required this.name, required this.id});
-
-  factory _$PokemonShortEntityImpl.fromJson(Map<String, dynamic> json) =>
-      _$$PokemonShortEntityImplFromJson(json);
 
   @override
   final String name;
@@ -69,9 +62,6 @@ abstract class _PokemonShortEntity implements PokemonShortEntity {
   const factory _PokemonShortEntity(
       {required final String name,
       required final String id}) = _$PokemonShortEntityImpl;
-
-  factory _PokemonShortEntity.fromJson(Map<String, dynamic> json) =
-      _$PokemonShortEntityImpl.fromJson;
 
   @override
   String get name;
