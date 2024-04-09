0ba6a16a52e2e2f7a201bf92deb7fd0b7d8dc2ec
kirilla <116667638+KirillStolbov@users.noreply.github.com>
2024-04-09 14:13:32 +0300

    usecase params refactoring

--- lib/features/pokemons_info/domain/repositories/pokemons_info_repository.dart
+++ lib/features/pokemons_info/domain/repositories/pokemons_info_repository.dart
@@ -1,4 +1,5 @@
 import '../../../../shared/domain/entities/api_response.dart';
+import '../entities/params/pokemons_long_params.dart';
 import '../entities/params/pokemons_short_params.dart';
 import '../entities/pokemon_long/pokemon_long_entity.dart';
 import '../entities/pokemon_short/pokemon_short_entity.dart';
@@ -6,5 +7,5 @@ import '../entities/pokemon_short/pokemon_short_entity.dart';
 abstract interface class PokemonsInfoRepository {
   Future<ApiResponse<List<PokemonShortEntity>>> readPokemonsShort(PokemonsShortParams params);
 
-  Future<ApiResponse<PokemonLongEntity>> readPokemonLong(String id);
+  Future<ApiResponse<PokemonLongEntity>> readPokemonLong(PokemonLongParams params);
 }
--- lib/features/pokemons_info/domain/usecases/read_pokemon_long_usecase.dart
+++ lib/features/pokemons_info/domain/usecases/read_pokemon_long_usecase.dart
@@ -1,13 +1,14 @@
 import '../../../../shared/domain/entities/api_response.dart';
 import '../../../../shared/domain/usecases/usecase.dart';
+import '../entities/params/pokemons_long_params.dart';
 import '../entities/pokemon_long/pokemon_long_entity.dart';
 import '../repositories/pokemons_info_repository.dart';
 
-final class ReadPokemonLongUseCase implements UseCase<PokemonLongEntity, String> {
+final class ReadPokemonLongUseCase implements UseCase<PokemonLongEntity, PokemonLongParams> {
   const ReadPokemonLongUseCase(this.repository);
 
   final PokemonsInfoRepository repository;
 
   @override
-  Future<ApiResponse<PokemonLongEntity>> invoke(String params) => repository.readPokemonLong(params);
+  Future<ApiResponse<PokemonLongEntity>> invoke(PokemonLongParams params) => repository.readPokemonLong(params);
 }
