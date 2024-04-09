5eda640150c27f3010a02ca44bea936bfa3859a2
kirilla <116667638+KirillStolbov@users.noreply.github.com>
2024-04-09 18:47:11 +0300

    global error handling setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import 'core/router/router.dart';
 
+// dart run build_runner build --delete-conflicting-outputs
+
 void main() {
+  WidgetsBinding.instance.platformDispatcher.onError = (error, stack) {
+    log('Top level error', error: error, stackTrace: stack);
+
+    return true;
+  };
+
+  FlutterError.onError = (details) {
+    if (!details.silent) {
+      log('Top level flutter error', error: details.exception, stackTrace: details.stack);
+    }
+  };
+
   runApp(const MainApp());
 }
 
